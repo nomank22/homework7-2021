8e0c88bd98f7c03c6a89df35bd83e447f3e68bea
nomank22 <78188090+nomank22@users.noreply.github.com>
2021-04-07 23:04:40 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -1,11 +1,11 @@
-var video;
+var video = document.querySelector("#player1")
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
-
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+video.play()
+});
